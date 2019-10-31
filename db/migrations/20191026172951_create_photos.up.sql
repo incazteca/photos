a010e1a05be1220b77840feaf5883a7fcd7baf95
Franco Reyes <freyes1988@gmail.com>
2019-10-30 23:36:24 -0500

    Update migration with proper column name

--- db/migrations/20191026172951_create_photos.up.sql
+++ db/migrations/20191026172951_create_photos.up.sql
@@ -2,7 +2,7 @@ CREATE TABLE photos(
   id serial PRIMARY KEY,
   location varchar NOT NULL,
   original boolean DEFAULT false NOT NULL,
-  attrs jsonb DEFAULT '{}'::jsonb NOT NULL
+  exif jsonb DEFAULT '{}'::jsonb NOT NULL
 );
 
-CREATE INDEX photos_attr_idx ON photos USING gin (attrs);
+CREATE INDEX photos_attr_idx ON photos USING gin (exif);
