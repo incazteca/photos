5905319d95647dd0c6d68b63b50aecd4a261d423
Franco Reyes <incazteca12345@gmail.com>
2020-02-19 21:36:09 -0600

    Add photo related routes

---
+++ routes/photo.go
@@ -0,0 +1,70 @@
+package routes
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/incazteca/photos/services"
+	"html/template"
+	"net/http"
+)
+
+type PhotoHandler struct {
+	photoService services.PhotoService
+}
+
+//NewPhotoHandler return a new handler for photos
+func NewPhotoHandler(mux *http.ServeMux, ps services.PhotoService) PhotoHandler {
+	handler := PhotoHandler{ps}
+
+	mux.HandleFunc("/", handler.getPhotos)
+	mux.HandleFunc("/photo", handler.handlePhoto)
+	return handler
+}
+
+func (handler *PhotoHandler) getPhotos(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
+	if r.Method == "GET" {
+		photos, err := handler.photoService.FetchAll()
+
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+
+		t = template.Must(
+			template.ParseFiles(
+				"templates/common/head.tmpl",
+				"templates/photos/index.tmpl",
+				"templates/common/footer.tmpl",
+			),
+		)
+		err = t.ExecuteTemplate(w, "index.tmpl", photos)
+
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	}
+}
+
+func (handler *PhotoHandler) handlePhoto(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/photo" {
+		http.NotFound(w, r)
+		return
+	}
+
+	if r.Method == "POST" {
+		recordID, err := handler.StorePhoto(r.Body)
+
+		if err != nil {
+			fmt.Println(err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+
+		w.WriteHeader(http.StatusOK)
+		json.NewEncoder(w).Encode(SuccessCreate{recordID})
+		return
+	}
+}
